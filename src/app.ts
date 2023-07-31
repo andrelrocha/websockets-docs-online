933437697c6e77e08126cf66132f4d5ee220f91c
andrelrocha <andrerocha0911@gmail.com>
2023-07-31 08:04:34 -0300

    deploying repo

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import url from 'url'
+import path from 'path'
+
+import { db } from "./db/dbConnect";
+
+db.on("error", console.log.bind(console, "Database connection error!"));
+db.once("open", () => console.log("Database connected!"));
+
+const app = express()
+app.use(express.json())
+
+// Obtém o caminho do arquivo atual
+const currentPath = path.dirname(__filename)
+// Navega até o diretório 'public' a partir do diretório atual
+const publicDir = path.join(currentPath, "..", "public");
+// Utiliza o diretório 'public' para servir arquivos estáticos
+app.use(express.static(publicDir));
+
+export { app }
