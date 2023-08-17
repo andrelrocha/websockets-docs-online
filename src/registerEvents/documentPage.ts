ec89133b6d785b28abd8ce39cb7a5aee9b7aae02
andrelrocha <andrerocha0911@gmail.com>
2023-08-17 11:22:59 -0300

    refactor: refatorando para deixar os eventos da page de document exclusivos para o evento selectDocument

--- src/registerEvents/documentPage.ts
+++ src/registerEvents/documentPage.ts
@@ -4,14 +4,6 @@ import { findDocument, updateTextEditor } from "../UseCases/document/DocumentsDb
 import { addConnection, getUsersDocument } from "../UseCases/utils/documentConnections"
 
 function registerEventsDocument(socket, io) {
-    socket.on("deleteDocument", async ( documentName ) => {
-        const result = await deleteDocument(documentName)
-
-        if (result) {
-            io.emit("deleteDocumentClientsInterface", documentName)
-        }
-    })
-
     socket.on("selectDocument", async ({ documentName, userName }, returnName ) => {
         const document = await findDocument(documentName)
 
@@ -26,19 +18,26 @@ function registerEventsDocument(socket, io) {
 
             delayedReturnName(document.text, returnName);
         }
-    })
 
-    socket.on("textEditor", async ({ text, documentName }) => {    
-        const update = await updateTextEditor(documentName, text)
+        socket.on("textEditor", async ({ text, documentName }) => {    
+            const update = await updateTextEditor(documentName, text)
+    
+            if (update.modifiedCount > 0) {
+                socket.to(documentName).emit("textEditorClients", text)
+            }
+        })
+    
+        socket.on("deleteDocument", async ( documentName ) => {
+            const result = await deleteDocument(documentName)
+    
+            if (result) {
+                io.emit("deleteDocumentClientsInterface", documentName)
+            }
+        })
 
-        if (update.modifiedCount > 0) {
-            socket.to(documentName).emit("textEditorClients", text)
-        }
-
-        //manda para todos os clientes conectados no socket
-        //socket.broadcast.emit("textEditorClients", text)
-        //manda para todos os clientes conectados na sala
-        //io.to(documentName).emit("textEditorClients", text)
+        socket.on("disconnect", () => {
+            
+        })
     })
 }
 
